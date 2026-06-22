0203d75ff8cbe1f073f098203dca9af4a96bf3be
superroket169 <isagullu2010@gmail.com>
2026-06-22 12:43:35 +0300

    feat: add tensor sketch

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,5 +1,7 @@
 use crate::context::WgpuContext;
+use bytemuck::Pod;
 use std::sync::Arc;
+use wgpu::util::DeviceExt;
 
 pub struct Tensor {
     pub ctx: Arc<WgpuContext>,
@@ -22,5 +24,70 @@ impl Tensor {
             buffer: Arc::new(buffer),
             size: size_bytes,
         }
+    }
+
+    pub fn init_from_cpu<T: Pod>(
+        ctx: Arc<WgpuContext>,
+        data: &[T],
+        usage: wgpu::BufferUsages,
+    ) -> Self {
+        let size = (data.len() * std::mem::size_of::<T>()) as wgpu::BufferAddress;
+
+        let buffer = ctx
+            .device
+            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Wilupgu_Tensor_Init"),
+                contents: bytemuck::cast_slice(data),
+                usage: usage | wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::COPY_SRC,
+            });
+
+        Self {
+            ctx,
+            buffer: Arc::new(buffer),
+            size,
+        }
+    }
+
+    pub fn copy_from_cpu<T: Pod>(&self, data: &[T]) {
+        self.ctx
+            .queue
+            .write_buffer(&self.buffer, 0, bytemuck::cast_slice(data));
+    }
+
+    pub fn to_cpu<T: Pod + Default + Clone>(&self) -> Vec<T> {
+        let device = &self.ctx.device;
+        let queue = &self.ctx.queue;
+
+        let staging_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Staging_Buffer"),
+            size: self.size,
+            usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let mut encoder =
+            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        encoder.copy_buffer_to_buffer(&self.buffer, 0, &staging_buffer, 0, self.size);
+        queue.submit(Some(encoder.finish()));
+
+        let buffer_slice = staging_buffer.slice(..);
+        let (sender, receiver) = futures_intrusive::channel::shared::oneshot_channel();
+        buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
+
+        device.poll(wgpu::Maintain::Wait);
+        pollster::block_on(async { receiver.receive().await.unwrap().unwrap() });
+
+        let data = buffer_slice.get_mapped_range();
+        let result: Vec<T> = bytemuck::cast_slice(&data).to_vec();
+
+        drop(data);
+        staging_buffer.unmap();
+
+        result
+    }
+
+    pub fn free(self) {
+        self.buffer.destroy();
     }
 }
