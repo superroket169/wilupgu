b1ad935fcb6509e2ab580351cac174edb02b7a5f
superroket169 <isagullu2010@gmail.com>
2026-06-22 15:43:20 +0300

    feat: shaders.rs update

--- src/nn/shaders.rs
+++ src/nn/shaders.rs
@@ -2,6 +2,8 @@ use crate::graph::{ShaderDef, TensorMode};
 
 pub enum BuiltInShader {
     MatMul,
+    Embedding,
+    CausalMask,
     RMSNorm,
     SiLU,
     RoPE,
@@ -13,7 +15,7 @@ impl BuiltInShader {
     pub fn get_def(&self) -> ShaderDef {
         match self {
             Self::MatMul => ShaderDef::new(
-                "MatMul",
+                "MatMul_L1_Tiled",
                 include_str!("../shaders/matmul.wgsl"),
                 vec![
                     TensorMode::Input,
@@ -21,6 +23,21 @@ impl BuiltInShader {
                     TensorMode::Output,
                     TensorMode::Meta,
                 ],
+            ),
+            Self::Embedding => ShaderDef::new(
+                "Embedding",
+                include_str!("../shaders/embedding.wgsl"),
+                vec![
+                    TensorMode::Input,
+                    TensorMode::Input,
+                    TensorMode::Output,
+                    TensorMode::Meta,
+                ],
+            ),
+            Self::CausalMask => ShaderDef::new(
+                "CausalMask",
+                include_str!("../shaders/causal_mask.wgsl"),
+                vec![TensorMode::InOut, TensorMode::Meta],
             ),
             Self::RMSNorm => ShaderDef::new(
                 "RMSNorm",
