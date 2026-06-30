31ee8474ac8f946d2c4f6a65c23210a6310047b9
superroket169 <isagullu2010@gmail.com>
2026-06-30 21:56:28 +0300

    update: mod and lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
-pub mod context;
+pub mod backend;
+pub mod backends;
 pub mod graph;
 pub mod nn;
 pub mod tensor;
 
-pub use context::WgpuContext;
-pub use graph::{ComputeGraph, ComputeNode};
+pub use backend::{Backend, Binding, TensorMode};
+pub use backends::WgpuBackend;
+#[cfg(feature = "cuda")]
+pub use backends::CudaBackend;
+pub use graph::{fuse_compute_graphs, ComputeGraph};
 pub use tensor::Tensor;
 
 pub type Real = f32;
